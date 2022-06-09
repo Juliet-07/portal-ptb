b0e309ba9d673ea8c9cdec642cd32cc9790409ab
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2022-06-09 14:02:38 +0100

    consuming API

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Formik from "formik";
 import Head from "next/head";
 import Link from "next/link";
 import { FaRegEnvelope, FaSignInAlt } from "react-icons/fa";
 import { MdLockOutline } from "react-icons/md";
 
 const Home = () => {
+  const onSubmit = () => {
+    try {
+      fetch("https://192.168.201.46/Intranet/User/login-user", {
+        method: "POST",
+        body: JSON.stringify({
+          username: "",
+          password: "",
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  const initialValues = {
+    username: "",
+    password: "",
+  };
   return (
     <div className="ptbPortalHome">
       <Head>
@@ -17,32 +41,56 @@ const Home = () => {
       </h1>
       {/* content */}
       <div className="sub-main-w3">
-        <form>
-          <p className="legend">Welcome</p>
-          <div className=" w-100 bg-red-100 p-2 flex items-center mb-3">
-            <FaRegEnvelope className="text-black m-2" />
-            <input
-              name="email"
-              type="email"
-              placeholder="Email"
-              className="bg-gray-100 outline-none text-sm flex-1"
-            />
-          </div>
-          <div className=" w-100 bg-red-100 p-2 flex items-center mb-3">
-            <MdLockOutline className="text-black m-2" />
-            <input
-              name="password"
-              type="password"
-              placeholder="Password"
-              className="bg-gray-100 outline-none text-sm flex-1"
-            />
-          </div>
-          <Link href="/admin/dashboard">
-            <button type="submit" className="submit">
-              <FaSignInAlt />
-            </button>
-          </Link>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validate={(values) => {
+            const errors = {};
+            if (!values.username) {
+              errors.username = "required";
+            } else if (
+              /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.username)
+            ) {
+              errors.username = "Invalid user";
+            }
+            return errors;
+          }}
+          // onSubmit={() => {}}
+        >
+          {({ values, errors, touched, handleChange, handleSubmit }) => (
+            <form>
+              <p className="legend">Welcome</p>
+              <div className=" w-100 bg-red-100 p-2 flex items-center mb-3">
+                <FaRegEnvelope className="text-black m-2" />
+                <input
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  className="bg-gray-100 outline-none text-sm flex-1"
+                  onChange={handleChange}
+                  value={values.username}
+                />
+                {errors.username && touched.username && errors.username}
+              </div>
+              <div className=" w-100 bg-red-100 p-2 flex items-center mb-3">
+                <MdLockOutline className="text-black m-2" />
+                <input
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  className="bg-gray-100 outline-none text-sm flex-1"
+                  onChange={handleChange}
+                  value={values.password}
+                />
+                {errors.password && touched.password && errors.password}
+              </div>
+              {/* <Link href="/admin/dashboard"> */}
+              <button type="submit" className="submit" onClick={onSubmit}>
+                <FaSignInAlt />
+              </button>
+              {/* </Link> */}
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
