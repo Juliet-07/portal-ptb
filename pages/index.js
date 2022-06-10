d04dc0a43a5203768ca82d278401f500a31a3d0b
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2022-06-10 15:19:46 +0100

    integrating with Active Directory

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from "react";
-import Formik from "formik";
 import Head from "next/head";
 import Link from "next/link";
 import { FaRegEnvelope, FaSignInAlt } from "react-icons/fa";
 import { MdLockOutline } from "react-icons/md";
 
 const Home = () => {
-  const onSubmit = () => {
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+  const [profile, setProfile] = useState(initialValues);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setProfile({ ...profile, [name]: value });
+  };
+
+  const login = () => {
     try {
-      fetch("https://192.168.201.46/Intranet/User/login-user", {
+      let url = "https://192.168.201.46/Intranet/User/login-user";
+      console.log(url);
+      fetch(url, {
         method: "POST",
         body: JSON.stringify({
-          username: "",
-          password: "",
+          username: profile.username,
+          password: profile.password,
         }),
         headers: {
           "Content-type": "application/json; charset=UTF-8",
@@ -24,10 +36,9 @@ const Home = () => {
       console.log(err.message);
     }
   };
-  const initialValues = {
-    username: "",
-    password: "",
-  };
+
+  useEffect(() => {});
+
   return (
     <div className="ptbPortalHome">
       <Head>
@@ -41,56 +52,40 @@ const Home = () => {
       </h1>
       {/* content */}
       <div className="sub-main-w3">
-        <Formik
-          initialValues={initialValues}
-          validate={(values) => {
-            const errors = {};
-            if (!values.username) {
-              errors.username = "required";
-            } else if (
-              /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.username)
-            ) {
-              errors.username = "Invalid user";
-            }
-            return errors;
-          }}
-          // onSubmit={() => {}}
-        >
-          {({ values, errors, touched, handleChange, handleSubmit }) => (
-            <form>
-              <p className="legend">Welcome</p>
-              <div className=" w-100 bg-red-100 p-2 flex items-center mb-3">
-                <FaRegEnvelope className="text-black m-2" />
-                <input
-                  name="username"
-                  type="text"
-                  placeholder="Username"
-                  className="bg-gray-100 outline-none text-sm flex-1"
-                  onChange={handleChange}
-                  value={values.username}
-                />
-                {errors.username && touched.username && errors.username}
-              </div>
-              <div className=" w-100 bg-red-100 p-2 flex items-center mb-3">
-                <MdLockOutline className="text-black m-2" />
-                <input
-                  name="password"
-                  type="password"
-                  placeholder="Password"
-                  className="bg-gray-100 outline-none text-sm flex-1"
-                  onChange={handleChange}
-                  value={values.password}
-                />
-                {errors.password && touched.password && errors.password}
-              </div>
-              {/* <Link href="/admin/dashboard"> */}
-              <button type="submit" className="submit" onClick={onSubmit}>
-                <FaSignInAlt />
-              </button>
-              {/* </Link> */}
-            </form>
-          )}
-        </Formik>
+        <form>
+          <p className="legend">Welcome</p>
+          <div className=" w-100 bg-red-100 p-2 flex items-center mb-3">
+            <FaRegEnvelope className="text-black m-2" />
+            <input
+              name="username"
+              type="text"
+              placeholder="Username"
+              className="bg-gray-100 outline-none text-sm flex-1"
+              onChange={handleChange}
+            />
+          </div>
+          <div className=" w-100 bg-red-100 p-2 flex items-center mb-3">
+            <MdLockOutline className="text-black m-2" />
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              className="bg-gray-100 outline-none text-sm flex-1"
+              onChange={handleChange}
+            />
+          </div>
+          <Link href="/admin/dashboard">
+            <a
+              type="submit"
+              className="submit"
+              onClick={() => {
+                login();
+              }}
+            >
+              <FaSignInAlt />
+            </a>
+          </Link>
+        </form>
       </div>
     </div>
   );
